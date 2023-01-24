51faa9d0602aa0fb5e636b0f59a0d0f6d85ea7ac
Dark11296 <35867750+Dark11296@users.noreply.github.com>
2023-01-24 10:50:17 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+# LANG=en_US.UTF-8
+
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+purple(){
+    echo -e "\033[35m\033[01m$1\033[0m"
+}
+
+function input_ver(){
+	clear
+	purple " 请输入mdserver-web 版本号。留空则使用默认版本号0.11.4！"
+	yellow " ————————————————————————————————————————————————————"
+	echo
+	read -p "请输入版本号：" MenuInput
+	
+	if [ "$MenuInput" = "" ]; then
+		g_ver="0.11.4"
+	else
+		g_ver="${MenuInput}"
+	fi
+}
+
+input_ver "first"
+
+is64bit=`getconf LONG_BIT`
+
+if [ -f /etc/motd ];then
+    echo "welcome to mdserver-web panel" > /etc/motd
+fi
+
+startTime=`date +%s`
+
+_os=`uname`
+echo "use system: ${_os}"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root!"
+  exit
+fi
+
+if [ ${_os} == "Darwin" ]; then
+	OSNAME='macos'
+elif grep -Eq "openSUSE" /etc/*-release; then
+	OSNAME='opensuse'
+	zypper refresh
+elif grep -Eq "FreeBSD" /etc/*-release; then
+	OSNAME='freebsd'
+elif grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+	OSNAME='centos'
+	yum install -y wget zip unzip
+elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+	OSNAME='fedora'
+	yum install -y wget zip unzip
+elif grep -Eqi "Rocky" /etc/issue || grep -Eq "Rocky" /etc/*-release; then
+	OSNAME='rocky'
+	yum install -y wget zip unzip
+elif grep -Eqi "AlmaLinux" /etc/issue || grep -Eq "AlmaLinux" /etc/*-release; then
+	OSNAME='alma'
+	yum install -y wget zip unzip
+elif grep -Eqi "Amazon Linux" /etc/issue || grep -Eq "Amazon Linux" /etc/*-release; then
+	OSNAME='amazon'
+	yum install -y wget zip unzip
+elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+	OSNAME='debian'
+	apt update -y
+	apt install -y devscripts
+	apt install -y wget zip unzip
+elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+	OSNAME='ubuntu'
+	apt install -y wget zip unzip
+else
+	OSNAME='unknow'
+fi
+
+
+if [ $OSNAME != "macos" ];then
+	if id www &> /dev/null ;then 
+	    echo ""
+	else
+	    groupadd www
+		useradd -g www -s /bin/bash www
+	fi
+
+	mkdir -p /www/server
+	mkdir -p /www/wwwroot
+	mkdir -p /www/wwwlogs
+	mkdir -p /www/backup/database
+	mkdir -p /www/backup/site
+
+
+	# https://cdn.jsdelivr.net/gh/midoks/mdserver-web@latest/scripts/install.sh
+
+	if [ ! -d /www/server/mdserver-web ];then
+
+		cn=$(curl -fsSL -m 10 http://ipinfo.io/json | grep "\"country\": \"CN\"")
+		if [ ! -z "$cn" ];then
+			curl -sSLo /tmp/master.zip https://github.com/midoks/mdserver-web/archive/refs/tags/${g_ver}.zip
+		else
+			curl -sSLo /tmp/master.zip https://github.com/midoks/mdserver-web/archive/refs/tags/${g_ver}.zip
+		fi
+
+		cd /tmp && unzip /tmp/master.zip
+		mv -f /tmp/mdserver-web-${g_ver} /www/server/mdserver-web
+		rm -rf /tmp/master.zip
+		rm -rf /tmp/mdserver-web-${g_ver}
+	fi
+	
+	# install acme.sh
+	if [ ! -d /root/.acme.sh ];then
+	    if [ ! -z "$cn" ];then
+	        curl -sSL -o /tmp/acme.tar.gz https://ghproxy.com/github.com/acmesh-official/acme.sh/archive/master.tar.gz
+	        tar xvzf /tmp/acme.tar.gz -C /tmp
+	        cd /tmp/acme.sh-master
+	        bash acme.sh install
+	        cd -
+	    fi
+
+	    if [ ! -d /root/.acme.sh ];then
+	        curl  https://get.acme.sh | sh
+	    fi
+	fi
+fi
+
+echo "use system version: ${OSNAME}"
+cd /www/server/mdserver-web && bash scripts/install/${OSNAME}.sh
+
+
+cd /www/server/mdserver-web && bash cli.sh start
+isStart=`ps -ef|grep 'gunicorn -c setting.py app:app' |grep -v grep|awk '{print $2}'`
+n=0
+while [ ! -f /etc/rc.d/init.d/mw ];
+do
+    echo -e ".\c"
+    sleep 1
+    let n+=1
+    if [ $n -gt 20 ];then
+    	echo -e "start mw fail"
+    	exit 1
+    fi
+done
+
+cd /www/server/mdserver-web && bash /etc/rc.d/init.d/mw stop
+cd /www/server/mdserver-web && bash /etc/rc.d/init.d/mw start
+cd /www/server/mdserver-web && bash /etc/rc.d/init.d/mw default
+
+sleep 2
+if [ ! -e /usr/bin/mw ]; then
+	if [ -f /etc/rc.d/init.d/mw ];then
+		ln -s /etc/rc.d/init.d/mw /usr/bin/mw
+	fi
+fi
+
+endTime=`date +%s`
+((outTime=(${endTime}-${startTime})/60))
+echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
+
