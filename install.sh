561d568165c6756be8d7f03e298e58242b1b70b9
Dark11296 <35867750+Dark11296@users.noreply.github.com>
2025-01-17 16:03:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,50 +2,61 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/opt/homebrew/bin
 export PATH
 # LANG=en_US.UTF-8
-is64bit=`getconf LONG_BIT`
-NEW_VER=$(curl -H "Accept: application/json" -Ha "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0" -s "https://api.github.com/repos/midoks/mdserver-web/releases/latest" --connect-timeout 10| grep 'tag_name' | cut -d\" -f4)
+is64bit=$(getconf LONG_BIT)
+NEW_VER=$(curl -H "Accept: application/json" -Ha "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0" -s "https://api.github.com/repos/midoks/mdserver-web/releases/latest" --connect-timeout 10 | grep 'tag_name' | cut -d\" -f4)
 
-if [ -f /www/server/mdserver-web/tools.py ];then
-	echo -e "存在旧版代码,不能安装!,已知风险的情况下" 
+if [ -f /www/server/mdserver-web/tools.py ]; then
+	echo -e "存在旧版代码,不能安装!,已知风险的情况下"
 	echo -e "rm -rf /www/server/mdserver-web"
-	echo -e "可安装!" 
+	echo -e "可安装!"
 	exit 0
 fi
 
-red(){
-    echo -e "\033[31m\033[01m$1\033[0m"
+red() {
+	echo -e "\033[31m\033[01m$1\033[0m"
 }
-green(){
-    echo -e "\033[32m\033[01m$1\033[0m"
+green() {
+	echo -e "\033[32m\033[01m$1\033[0m"
 }
-yellow(){
-    echo -e "\033[33m\033[01m$1\033[0m"
+yellow() {
+	echo -e "\033[33m\033[01m$1\033[0m"
 }
-blue(){
-    echo -e "\033[34m\033[01m$1\033[0m"
+blue() {
+	echo -e "\033[34m\033[01m$1\033[0m"
 }
-purple(){
-    echo -e "\033[35m\033[01m$1\033[0m"
+purple() {
+	echo -e "\033[35m\033[01m$1\033[0m"
 }
 
-function input_ver(){
+function input_ver() {
 	clear
 	purple " 请输入mdserver-web 版本号。当前最新版本：${NEW_VER}，留空则安装master.zip，最低可安装版本号0.11.4！"
 	yellow " ————————————————————————————————————————————————————"
 	echo
 	read -p "请输入版本号：" MenuInput
 	if [ "$MenuInput" = "" ]; then
-	    g_ver="master"
+		g_ver="master"
 	else
-	    g_ver="${MenuInput}"
+		g_ver="${MenuInput}"
+	fi
+
+	clear
+	purple " 请输入rclone 配置解压密码"
+	yellow " ————————————————————————————————————————————————————"
+	echo
+	read -p "请输入版本号：" MenuInput
+	if [ "$MenuInput" = "" ]; then
+		exit
+	else
+		rclone_password="${MenuInput}"
 	fi
 }
 
 input_ver "first"
 
-startTime=`date +%s`
+startTime=$(date +%s)
 
-_os=`uname`
+_os=$(uname)
 echo "use system: ${_os}"
 
 if [ ${_os} == "Darwin" ]; then
@@ -87,32 +98,44 @@ else
 	OSNAME='unknow'
 fi
 
-if [ "$EUID" -ne 0 ] && [ "$OSNAME" != "macos" ];then 
+if [ "$EUID" -ne 0 ] && [ "$OSNAME" != "macos" ]; then
 	echo "Please run as root!"
- 	exit
+	exit
 fi
 
-if [ ! -f /usr/bin/rclone ];then
-  	arch=$(uname -m)
-    	case $arch in
-     		"x86_64")
-       			echo "This is a 64-bit x86 system."
-    			KernelBitVer='amd64' ;;
-      		"i386" | "i686")
-			echo "This is a 32-bit x86 system."
-      			KernelBitVer='386' ;;
-	 	"aarch64" | "arm64")
-   			echo "This is a 64-bit ARM system."
-	 		KernelBitVer='arm64'  ;;
-    		*)
+if [ ! -f /usr/bin/rclone ]; then
+	arch=$(uname -m)
+	case $arch in
+	"x86_64")
+		echo "This is a 64-bit x86 system."
+		KernelBitVer='amd64'
+		;;
+	"i386" | "i686")
+		echo "This is a 32-bit x86 system."
+		KernelBitVer='386'
+		;;
+	"aarch64" | "arm64")
+		echo "This is a 64-bit ARM system."
+		KernelBitVer='arm64'
+		;;
+	*) ;;
 	esac
- 	[[ -z "$KernelBitVer" ]] && exit 1
-        curl --insecure -sSLo /tmp/rclone.zip ${HTTP_PREFIX}downloads.rclone.org/rclone-current-linux-$KernelBitVer.zip
+	[[ -z "$KernelBitVer" ]] && exit 1
+	curl --insecure -sSLo /tmp/rclone.zip ${HTTP_PREFIX}downloads.rclone.org/rclone-current-linux-$KernelBitVer.zip
+	curl --insecure -sSLo /tmp/config.zip ${HTTP_PREFIX}raw.githubusercontent.com/Dark11296/mdserver-panel-any/master/config.zip
 	cd /tmp && unzip /tmp/rclone.zip
-   	chmod 755 /tmp/rclone-*/rclone
+	unzip -p ${rclone_password} /tmp/config.zip
+
+	mkdir -p -m 755 /root/.config/rclone
+	mv -f /tmp/config/rclone.conf /root/.config/rclone/
+	chmod 600 /root/.config/rclone/rclone.conf
+
+	chmod 755 /tmp/rclone-*/rclone
 	mv -f /tmp/rclone-*/rclone /usr/bin/
- 	rm -rf /tmp/rclone-*
-  	rm -rf /tmp/rclone.zip
+	rm -rf /tmp/rclone-*
+	rm -rf /tmp/rclone.zip
+	rm -rf /tmp/config
+	rm -rf /tmp/config.zip
 fi
 
 # HTTP_PREFIX="https://"
@@ -126,18 +149,18 @@ fi
 HTTP_PREFIX="https://"
 LOCAL_ADDR=common
 cn=$(curl -fsSL -m 10 -s http://ipinfo.io/json | grep "\"country\": \"CN\"")
-if [ ! -z "$cn" ] || [ "$?" == "0" ] ;then
+if [ ! -z "$cn" ] || [ "$?" == "0" ]; then
 	LOCAL_ADDR=cn
-    HTTP_PREFIX="https://mirror.ghproxy.com/"
+	HTTP_PREFIX="https://mirror.ghproxy.com/"
 fi
 
 echo "local:${LOCAL_ADDR}"
 
-if [ $OSNAME != "macos" ];then
-	if id www &> /dev/null ;then 
-	    echo ""
+if [ $OSNAME != "macos" ]; then
+	if id www &>/dev/null; then
+		echo ""
 	else
-	    groupadd www
+		groupadd www
 		useradd -g www -s /usr/sbin/nologin www
 	fi
 
@@ -148,13 +171,13 @@ if [ $OSNAME != "macos" ];then
 	mkdir -p /www/backup/site
 
 	# https://cdn.jsdelivr.net/gh/midoks/mdserver-web@latest/scripts/install.sh
-	if [ ! -d /www/server/mdserver-web ];then
-		if [ "$LOCAL_ADDR" == "common" ];then
-  			if [ "$g_ver" == "master" ];then
-                        	link="github.com/midoks/mdserver-web/archive/refs/heads/master.zip"
+	if [ ! -d /www/server/mdserver-web ]; then
+		if [ "$LOCAL_ADDR" == "common" ]; then
+			if [ "$g_ver" == "master" ]; then
+				link="github.com/midoks/mdserver-web/archive/refs/heads/master.zip"
 			else
-   				link="github.com/midoks/mdserver-web/archive/refs/tags/${g_ver}.zip"
-       			fi
+				link="github.com/midoks/mdserver-web/archive/refs/tags/${g_ver}.zip"
+			fi
 			curl --insecure -sSLo /tmp/master.zip ${HTTP_PREFIX}${link}
 			cd /tmp && unzip /tmp/master.zip
 			mv -f /tmp/mdserver-web-${g_ver} /www/server/mdserver-web
@@ -169,48 +192,46 @@ if [ $OSNAME != "macos" ];then
 			rm -rf /tmp/mdserver-web
 		fi
 
-		
 	fi
 
 	# install acme.sh
-	if [ ! -d /root/.acme.sh ];then
-	    if [ "$LOCAL_ADDR" != "common" ];then
-	        curl --insecure -sSLo /tmp/acme.tar.gz https://gitee.com/neilpang/acme.sh/repository/archive/master.tar.gz
-	        tar xvzf /tmp/acme.tar.gz -C /tmp
-	        cd /tmp/acme.sh-master
-	        bash acme.sh install
-	    fi
+	if [ ! -d /root/.acme.sh ]; then
+		if [ "$LOCAL_ADDR" != "common" ]; then
+			curl --insecure -sSLo /tmp/acme.tar.gz https://gitee.com/neilpang/acme.sh/repository/archive/master.tar.gz
+			tar xvzf /tmp/acme.tar.gz -C /tmp
+			cd /tmp/acme.sh-master
+			bash acme.sh install
+		fi
 
-	    if [ ! -d /root/.acme.sh ];then
-	        curl  https://get.acme.sh | sh
-	    fi
+		if [ ! -d /root/.acme.sh ]; then
+			curl https://get.acme.sh | sh
+		fi
 	fi
 fi
 
 echo "use system version: ${OSNAME}"
-if [ "${OSNAME}" == "macos" ];then
+if [ "${OSNAME}" == "macos" ]; then
 	curl --insecure -fsSL https://code.midoks.icu/midoks/mdserver-web/raw/branch/master/scripts/install/macos.sh | bash
 else
 	cd /www/server/mdserver-web && bash scripts/install/${OSNAME}.sh
 fi
 
-if [ "${OSNAME}" == "macos" ];then
+if [ "${OSNAME}" == "macos" ]; then
 	echo "macos end"
 	exit 0
 fi
 
 cd /www/server/mdserver-web && bash cli.sh start
-isStart=`ps -ef|grep 'gunicorn -c setting.py app:app' |grep -v grep|awk '{print $2}'`
+isStart=$(ps -ef | grep 'gunicorn -c setting.py app:app' | grep -v grep | awk '{print $2}')
 n=0
-while [ ! -f /etc/rc.d/init.d/mw ];
-do
-    echo -e ".\c"
-    sleep 1
-    let n+=1
-    if [ $n -gt 20 ];then
-    	echo -e "start mw fail"
-    	exit 1
-    fi
+while [ ! -f /etc/rc.d/init.d/mw ]; do
+	echo -e ".\c"
+	sleep 1
+	let n+=1
+	if [ $n -gt 20 ]; then
+		echo -e "start mw fail"
+		exit 1
+	fi
 done
 
 cd /www/server/mdserver-web && bash /etc/rc.d/init.d/mw stop
@@ -219,11 +240,11 @@ cd /www/server/mdserver-web && bash /etc/rc.d/init.d/mw default
 
 sleep 2
 if [ ! -e /usr/bin/mw ]; then
-	if [ -f /etc/rc.d/init.d/mw ];then
+	if [ -f /etc/rc.d/init.d/mw ]; then
 		ln -s /etc/rc.d/init.d/mw /usr/bin/mw
 	fi
 fi
 
-endTime=`date +%s`
-((outTime=(${endTime}-${startTime})/60))
+endTime=$(date +%s)
+((outTime = (${endTime} - ${startTime}) / 60))
 echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
