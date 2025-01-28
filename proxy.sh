21f8d6d6657e44f7626f32d6f0e89b6961560934
Dark11296 <35867750+Dark11296@users.noreply.github.com>
2025-01-28 16:19:35 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-old_text="209.141.33.31"
-new_text="47.79.89.238"
+old_text="47.79.89.238"
+new_text="209.141.33.31"
 folder_path="/www/server/web_conf/nginx/proxy"
 find "$folder_path" -type f -name "*.*" | while read file; do
   if [ -f "$file" ]; then
