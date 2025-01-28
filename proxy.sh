1ecbb237b94cb5b3e4eaeeb6ce0c414f96a5b703
Dark11296 <35867750+Dark11296@users.noreply.github.com>
2025-01-28 18:43:26 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -4,7 +4,7 @@ new_text="47.79.89.238"
 folder_path="/www/server/web_conf/nginx/proxy"
 find "$folder_path" -type f -name "*.*" | while read file; do
   if [ -f "$file" ]; then
-    sed -i "s/$old_text/$new_text/g" "$file"
+    sed -i "s/$new_text/$old_text/g" "$file"
   fi
 done
 systemctl stop openresty
